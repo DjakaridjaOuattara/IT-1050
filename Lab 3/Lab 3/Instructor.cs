eceb0753283a018b810a71e4bedfb83b88fce086
DJAKARIDJA OUATTARA <ouattaradjakaridja@yahoo.fr>
2017-11-26 16:53:04 -0500

    lab3

---
+++ Lab 3/Lab 3/Instructor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    class Instructor
+    {
+        private string Name;
+        private string CourseName;
+
+
+        public Instructor(string Name, string CourseName)
+        {
+            this.Name = Name;
+            this.CourseName = CourseName;
+        }
+
+        public void SetStudentGrade(Student student, int Grade)
+        {
+            student.SetGrade(Grade);
+        }
+
+        public void Print()
+        {
+            System.Console.WriteLine(this.Name);
+            System.Console.WriteLine(this.CourseName);
+        }
+
+    }
+}
